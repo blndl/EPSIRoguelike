cf3dbc977363006842236f2c50c49562f4cd93e8
Shiranamur <ulysse.barois@gmail.com>
2024-09-16 16:28:56 +0200

    Added events.json structure player.py

---
+++ player.py
@@ -0,0 +1,15 @@
+class Player:
+    def __init__(self, name, energy=100, moral=100):
+        self.energy = energy
+        self.moral = moral
+        self.name = name
+        self.money = 0
+
+        "Inventory slot"
+        self.inventory_slot_1 = None
+        self.inventory_slot_2 = None
+        self.inventory_slot_3 = None
+        self.inventory_slot_4 = None
+
+        "bag"
+        self.bag = []
