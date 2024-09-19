800b424835fea0abf3bb9f126af430f6d66b0b9f
Antonio <antoniociorba@yahoo.com>
2024-09-19 13:32:04 +0200

    added name variable to class item

--- Items/item_gestion.py
+++ Items/item_gestion.py
@@ -1,7 +1,7 @@
 from Json.json import BaseEntity
 
 class Item(BaseEntity):
-    def __init__(self, item_id, description, price, moral, energy, itemshop, consommable, img):
+    def __init__(self, item_id, description, price, moral, energy, itemshop, consommable, img, name):
         self.item_id = item_id
         self.description = description
         self.price = price
@@ -10,13 +10,14 @@ class Item(BaseEntity):
         self.itemshop = itemshop
         self.consommable = consommable
         self.sprite_path = img
+        self.name = name
 
     # This method is used to create a list of instances of the class Item with the data from items
     @classmethod
     def load_items(cls, file_path):
         data = cls.load_entity(file_path)
         return {item_id: cls(
-            item_id, item_data['description'], item_data['price'], item_data['moral'], item_data['energy'], item_data['itemshop'], item_data['consommable'], item_data.get('image', 'Items/Sprites/luckyblock.png'))
+            item_id, item_data['description'], item_data['price'], item_data['moral'], item_data['energy'], item_data['itemshop'], item_data['consommable'], item_data.get('image', 'Items/Sprites/luckyblock.png'), item_data['name'])
                 for item_id, item_data in data['items'].items()}
 
 # the instances are the values of the dictionary, the keys are the object_id
